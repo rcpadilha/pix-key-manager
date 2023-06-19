b8ea86b124bd515d72ca96d5fcaaf45fc1d67ef1
Roberto Padilha <robertocesardasilva@gmail.com>
2023-06-19 17:40:13 -0300

    Initial Commit

--- PixKeyManager/Program.cs
+++ PixKeyManager/Program.cs
@@ -15,6 +15,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<PixKeyContext>(
     options => options.UseNpgsql(
         builder.Configuration.GetConnectionString("PixKeyDb")));
@@ -76,6 +81,12 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
